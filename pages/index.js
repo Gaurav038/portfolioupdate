38365bc8170d4cdf4f78e518387078d02b98fc79
Gaurav Singh <gauravkarki038@gmail.com>
2023-04-11 21:18:08 +0530

    updated UI

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Main from "@/components/Main";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      okkkkkkkkkkk man
+      <Head>
+        <title>Clint | Front-End Developer</title>
+        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }
